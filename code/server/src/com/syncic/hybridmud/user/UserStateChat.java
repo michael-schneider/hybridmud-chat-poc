999f9c825f838a85142237f638cdb5f9d7699585
Michael Schneider <michael.schneider@bbbaden.ch>
2017-11-15 21:11:06 +0100

    Prepared Chat server-part.

--- code/server/src/com/syncic/hybridmud/user/UserStateChat.java
+++ code/server/src/com/syncic/hybridmud/user/UserStateChat.java
@@ -1,24 +1,60 @@
 package com.syncic.hybridmud.user;
 
-import com.syncic.hybridmud.server.websocket.MudWebSocketServer;
-
 import java.text.MessageFormat;
 
 public class UserStateChat implements UserState {
 
     public UserStateChat(User user) {
-        user.send("<server>Welcome to the chat</server>");
-        user.send("<server>type 'bye' to disconnect</server>");
+        user.send("<server subsystem=\"server\">Welcome to the chat</server>");
+        user.send("<server subsystem=\"server\">type 'bye' to disconnect</server>");
     }
 
     @Override
     public boolean receiveMessage(User user, String message) {
-        if (message.trim().equals("bye")) {
-            user.send("<server>Bye!</server>");
+        String decodedMessage = decode(message);
+
+        if (isCommand(message)) {
+            final String command = getCommand(message);
+            switch (command) {
+                default:
+
+                    user.send(MessageFormat.format("<server subsystem=\"server\">Do not understand \"{0}\"</server>", StringEscapeUtils.escapeXml(command)ccommand));
+
+            }
+        }
+
+        if (message..equals("bye")){
+            user.send("<server subsystem=\"server\">Bye!</server>");
             return false;
         }
         user.send(MessageFormat.format("ChatState: [{0}]", message));
 
         return true;
     }
+
+    private boolean isCommand(String message) {
+        return message.startsWith("/");
+    }
+
+    /**
+     * Commands start with a slash. if it is a chat-message, the first slash is escaped.
+     * Whitespaces at start and end are removed.
+     *
+     * @param message the message to decode
+     * @return decoded message
+     */
+    private String decode(String message) {
+        String returnMessage = message.trim();
+        if (message.startsWith("\\/")) {
+            returnMessage = message.substring(1);
+        }
+
+        return returnMessage;
+    }
+
+    private String escapeXml(String message) {
+
+        return "";
+    }
+
 }
