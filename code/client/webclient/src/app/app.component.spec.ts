36f140d35182a069e783ef31a27a5d366225f605
Michael Schneider <michael.schneider@bbbaden.ch>
2017-12-21 16:41:55 +0100

    Fixed some Unittests

--- code/client/webclient/src/app/app.component.spec.ts
+++ code/client/webclient/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
@@ -12,34 +12,28 @@ import { CurrentUserMockService } from '../../testing/current-user-mock.service'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    const mudxmlMockService: MudxmlMockService = new MudxmlMockService();
+    const websocketMockService: WebsocketMockService = new WebsocketMockService();
+    const currentUserMockService: CurrentUserMockService = new CurrentUserMockService();
+
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       providers: [
-        { provide: WebsocketService, useValue: WebsocketMockService},
-        { provide: MudxmlService, useValue: MudxmlMockService},
-        { provide: CurrentUserService, useValue: CurrentUserMockService},
+        { provide: WebsocketService, useValue: websocketMockService },
+        { provide: MudxmlService, useValue: mudxmlMockService },
+        { provide: CurrentUserService, useValue: currentUserMockService },
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
